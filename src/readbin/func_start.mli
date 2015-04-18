fef8ef2cb1010b044452e283a5e8cab0a80fbe11
Tiffany Bao <tiffanybao@cmu.edu>
2015-04-18 15:52:29 -0400

    Specify exception

--- src/readbin/func_start.mli
+++ src/readbin/func_start.mli
@@ -1,8 +1,8 @@
 open Bap.Std
 
-exception Bad_user_input
+exception Bad_user_input of string
 
 val byteweight : string -> Addr.Set.t
-val usersource : string -> Addr.Set.t
+val user : string -> Addr.Set.t
 val symbols : string -> Addr.Set.t
 val ida : string -> Addr.Set.t
