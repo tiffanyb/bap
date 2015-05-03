a3a2777558d5f59b689dc3d779afb8fa60b2f82b
Tiffany Bao <tiffanybao@cmu.edu>
2015-05-03 17:28:54 -0400

    add ground truth file

--- src/readbin/ground_truth.mli
+++ src/readbin/ground_truth.mli
@@ -1,11 +1,4 @@
 open Bap.Std
 
-exception External_cmderr of string
-
-(* [from_unstripped_bin bin] returns the function start address set *)
-(* from the symbols in unstripped binary. *)
-val from_unstripped_bin : string -> Addr.Set.t
-
-(* [from_symbol_file symbolfile ~testbin] returns the function start *)
-(* address set from the symbols in symbolfile *)
-val from_symbol_file : string -> testbin:string -> Addr.Set.t
+(* with_file : output the ground truth of function starts *)
+val with_file : filename:string -> testbin:string -> Addr.Set.t
