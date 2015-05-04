a31d2469044ad17264ee14a365d79cfb70be99e7
Tiffany Bao <tiffanybao@cmu.edu>
2015-05-04 12:17:44 -0400

    change comment

--- src/readbin/func_start.mli
+++ src/readbin/func_start.mli
@@ -2,7 +2,7 @@ open Bap.Std
 
 exception External_cmderr of string
 
-(* eval : given the tool and test binary, output the function start *)
-(* set and the tool's short name for output table header *)
-(* [eval ~tool ~testbin] .... *)
+(* [eval ~tool ~testbin] evaluates the tool on the testbin binary, and *)
+(* returns the function start address set, and the tool's name for the *)
+(* header of output table *)
 val eval: tool:string -> testbin:string -> Addr.Set.t * string
