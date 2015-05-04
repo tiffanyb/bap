e81410bc843d55da96ea57210a072e908364057d
Tiffany Bao <tiffanybao@cmu.edu>
2015-05-04 12:29:16 -0400

    Change filename

--- src/readbin/fsi_benchmark.ml
+++ src/readbin/fsi_benchmark.ml
@@ -20,8 +20,7 @@ let tool : string Term.t =
   let doc = "The tool of the function start result that we are going to
   evaluate. If user wants to evaluate bap-byteweight, one can use
   \"bap-byteweight\". If user want to evaluate IDA, one can use
-  \"idaq\", \"idal\", \"idaq64\" or the specific path of IDA. The
-  default value is \"bap-byteweight\". " in
+  \"idaq\", \"idal\", \"idaq64\" or the specific path of IDA." in
   Arg.(required & pos 0 (some string) (Some "bap-byteweight") & info [] ~docv:"tool" ~doc)
 
 let bin : string Term.t =
@@ -33,7 +32,7 @@ let gt : string Term.t =
     "The ground truth. The ground truth can be an unstripped
   binary, or a .scm file with symbol information. In .scm file, each symbol should
   be in format of:
-  ([symbol name] [symbol start address] [symbol end address]), e.g.
+  (<symbol name> <symbol start address> <symbol end address>), e.g.
   (malloc@@GLIBC_2.4 0x11034 0x11038)" in
   Arg.(required & pos 2 (some non_dir_file) None
        & info [] ~docv:"ground-truth" ~doc)
@@ -53,20 +52,19 @@ let print_metrics : _ list Term.t =
        info ["with-metrics"; "c"] ~doc)
 
 let print {tool_name;fp;fn;tp;prec;recl;f_05} print_metrics =
-      (* print out the metrics *)
-    let headers, items =
-      let rev_hd, rev_it = List.fold print_metrics ~init:(["Tool"], [tool_name])
-          ~f:(fun (headers, items) -> function
-              | `with_prec -> "Prcs"::headers, (Printf.sprintf "%.2g" prec)::items
-              | `with_recl -> "Rcll"::headers, (Printf.sprintf "%.2g" recl)::items
-              | `with_F -> "F_05"::headers, (Printf.sprintf "%.2g" f_05)::items
-              | `with_TP -> "TP"::headers, (Printf.sprintf "%d" tp)::items
-              | `with_FN -> "FN"::headers, (Printf.sprintf "%d" fn)::items
-              | `with_FP -> "FP"::headers, (Printf.sprintf "%d" fp)::items ) in
-      List.rev rev_hd, List.rev rev_it in
-    Printf.printf "%s\n%s\n" (String.concat ~sep:"\t" headers)
-      (String.concat ~sep:"\t" items)
-
+  let headers, items =
+    let rev_hd, rev_it = List.fold print_metrics ~init:(["Tool"], [tool_name])
+        ~f:(fun (headers, items) -> function
+            | `with_prec -> "Prcs"::headers, (Printf.sprintf "%.2g" prec)::items
+            | `with_recl -> "Rcll"::headers, (Printf.sprintf "%.2g" recl)::items
+            | `with_F -> "F_05"::headers, (Printf.sprintf "%.2g" f_05)::items
+            | `with_TP -> "TP"::headers, (Printf.sprintf "%d" tp)::items
+            | `with_FN -> "FN"::headers, (Printf.sprintf "%d" fn)::items
+            | `with_FP -> "FP"::headers, (Printf.sprintf "%d" fp)::items ) in
+    List.rev rev_hd, List.rev rev_it in
+  Printf.printf "%s\n%s\n" (String.concat ~sep:"\t" headers)
+    (String.concat ~sep:"\t" items)
+    
 
 let get_format f =
   if Filename.check_suffix f ".scm" then `symbol_file
@@ -91,9 +89,7 @@ let compare tool bin gt print_metrics : unit = try
       let f_tp = float tp in
       f_tp /. (f_tp +. f_fp), f_tp /. (f_tp +. f_fn) in
     let f_05 = 1.5 *. prec *. recl /. (0.5 *. prec +. recl) in
-
     print {tool_name;fp;fn;tp;prec;recl;f_05} print_metrics
-      
   with
   | Func_start.External_cmderr cmd ->
     Printf.printf
@@ -106,7 +102,7 @@ let info =
   let doc = "to compare the functions start identification result to the ground
   truth" in
   let man = [] in
-  Term.info "bap-measure-byteweight" ~doc ~man
+  Term.info "bap-fsi-benchmark" ~doc ~man
 
 let () =
   Printexc.record_backtrace true;
