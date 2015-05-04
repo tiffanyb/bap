28d69a210132ece5fd16b931a1da78e325fb510b
Tiffany Bao <tiffanybao@cmu.edu>
2015-05-04 12:18:10 -0400

    change comment

--- src/readbin/ground_truth.mli
+++ src/readbin/ground_truth.mli
@@ -2,10 +2,10 @@ open Bap.Std
 
 exception External_cmderr of string
 
-(* [ from_unstripped_bin bin ] returns the function start address set *)
+(* [from_unstripped_bin bin] returns the function start address set *)
 (* from the symbols in unstripped binary. *)
 val from_unstripped_bin : string -> Addr.Set.t
 
-(* [ from_symbol_file symbolfile ~testbin ] returns the function start *)
+(* [from_symbol_file symbolfile ~testbin] returns the function start *)
 (* address set from the symbols in symbolfile *)
 val from_symbol_file : string -> testbin:string -> Addr.Set.t
